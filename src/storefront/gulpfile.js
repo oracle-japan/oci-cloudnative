23bc5795e03129931392ac8b5fa48d5c82a571c0
tniita <takuya.niita@oracle.com>
2025-03-01 23:32:13 +0900

    fix broken image

--- src/storefront/gulpfile.js
+++ src/storefront/gulpfile.js
@@ -1,23 +1,23 @@
-const gulp     = require('gulp'),
-  fs           = require('fs'),
-  path         = require('path'),
-  argv         = require('minimist')(process.argv.slice(2)),
-  less         = require('gulp-less'),
-  header       = require('gulp-header'),
-  rev          = require('gulp-rev'),
-  revReplace   = require('gulp-rev-replace'),
-  sync         = require('browser-sync'),
-  webpack      = require('webpack-stream'),
-  del          = require('del'),
-  imagemin     = require('gulp-imagemin'),
-  pngquant     = require('imagemin-pngquant'),
-  cache        = require('gulp-cache'),
+const gulp = require('gulp'),
+  fs = require('fs'),
+  path = require('path'),
+  argv = require('minimist')(process.argv.slice(2)),
+  less = require('gulp-less'),
+  header = require('gulp-header'),
+  rev = require('gulp-rev'),
+  revReplace = require('gulp-rev-replace'),
+  sync = require('browser-sync'),
+  webpack = require('webpack-stream'),
+  del = require('del'),
+  imagemin = require('gulp-imagemin'),
+  pngquant = require('imagemin-pngquant'),
+  cache = require('gulp-cache'),
   autoprefixer = require('autoprefixer'),
-  postcss      = require('gulp-postcss'),
-  csso         = require('gulp-csso'),
-  pug          = require('gulp-pug'),
-  proxy        = require('http-proxy-middleware'),
-  pkg          = require('./package.json');
+  postcss = require('gulp-postcss'),
+  csso = require('gulp-csso'),
+  pug = require('gulp-pug'),
+  proxy = require('http-proxy-middleware'),
+  pkg = require('./package.json');
 
 // load .env configurations
 require('dotenv').config();
@@ -49,14 +49,14 @@ const pugOpt = {
   }
 };
 
-gulp.task('html:pages', function() {
+gulp.task('html:pages', function () {
   return gulp.src('src/templates/pages/**/*.pug')
     .pipe(pug(pugOpt))
     .pipe(gulp.dest(opt.buildDir))
     .pipe(sync.stream({ once: true }))
 });
 
-gulp.task('html:views', function() {
+gulp.task('html:views', function () {
   return gulp.src('src/templates/views/**/*.pug')
     .pipe(pug(pugOpt))
     .pipe(gulp.dest(`${opt.buildDir}/views`))
@@ -66,7 +66,7 @@ gulp.task('html:views', function() {
 gulp.task('html', gulp.parallel('html:pages', 'html:views'));
 
 // Styles
-gulp.task('styles', function() {
+gulp.task('styles', function () {
   return gulp.src(['src/styles/**/*.less', '!src/styles/**/_*.less'])
     .pipe(less({ relativeUrls: true }))
     // .pipe(concat('style.css'))
@@ -80,7 +80,7 @@ gulp.task('styles', function() {
 });
 
 // Scripts
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
   const wpconf = require('./webpack.config');
   const { production } = argv;
   return gulp.src('src/scripts/*.js')
@@ -96,7 +96,7 @@ gulp.task('scripts', function() {
 });
 
 // Scripts Extras
-gulp.task('oda:copy', function() {
+gulp.task('oda:copy', function () {
   return gulp.src('src/scripts/oda/*')
     .pipe(gulp.dest(`${opt.buildDir}/scripts/oda`))
 });
@@ -106,18 +106,18 @@ gulp.task('extras', gulp.series('oda:copy'));
 // Images
 
 // copy images into build
-gulp.task('image:copy', function() {
+gulp.task('image:copy', function () {
   return gulp.src('src/images/**/*')
     .pipe(gulp.dest(`${opt.buildDir}/images`))
 });
 
 // optimize images 
-gulp.task('image:optimize', function(done) {
-  return gulp.src(`${opt.buildDir}/images/**/*`)
+gulp.task('image:optimize', function (done) {
+  return gulp.src(`${opt.buildDir}/images/**/*`, { encoding: false })
     .pipe(cache(imagemin([
-      pngquant({quality: [0.3, 0.5]}),
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.mozjpeg({progressive: true, quality: 55}),
+      pngquant({ quality: [0.3, 0.5] }),
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.mozjpeg({ progressive: true, quality: 55 }),
       imagemin.svgo({
         plugins: [
           { removeViewBox: true },
@@ -133,7 +133,7 @@ gulp.task('image:optimize', function(done) {
 gulp.task('images', gulp.series('image:copy', 'image:optimize'));
 
 // Copy
-gulp.task('copy', function() {
+gulp.task('copy', function () {
   return gulp.src([
     'src/*',
     '!src/images/*',
@@ -150,15 +150,15 @@ gulp.task('copy', function() {
 });
 
 // Revision
-gulp.task('rev', function() {
-  return gulp.src([`${opt.buildDir}/scripts/**/*.js`, `${opt.buildDir}/styles/**/*.css`], {base: opt.buildDir})
+gulp.task('rev', function () {
+  return gulp.src([`${opt.buildDir}/scripts/**/*.js`, `${opt.buildDir}/styles/**/*.css`], { base: opt.buildDir })
     .pipe(rev())
     .pipe(gulp.dest(opt.buildDir))
     .pipe(rev.manifest())
     .pipe(gulp.dest(opt.buildDir))
 });
 
-gulp.task('rev:replace', gulp.series('rev', function() {
+gulp.task('rev:replace', gulp.series('rev', function () {
   const manifest = gulp.src(`${opt.buildDir}/rev-manifest.json`);
   return gulp.src([`${opt.buildDir}/*.html`])
     .pipe(revReplace({ manifest }))
@@ -167,7 +167,7 @@ gulp.task('rev:replace', gulp.series('rev', function() {
 
 // Server
 
-gulp.task('server', function() {
+gulp.task('server', function () {
   // READ the API_PROXY environment variable for the shop services
   const { API_PROXY = 'http://localhost:8080' } = process.env;
   sync.init({
@@ -176,10 +176,10 @@ gulp.task('server', function() {
     port: 3000,
     ui: false,
     routes: { '/': 'index.html' },
-    middleware: [ proxy.createProxyMiddleware('/api', {
+    middleware: [proxy.createProxyMiddleware('/api', {
       target: API_PROXY,
       pathRewrite: { '^/api': '' },
-    }) ],
+    })],
     server: {
       baseDir: opt.buildDir
     }
@@ -188,31 +188,31 @@ gulp.task('server', function() {
 
 // Clean
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
   return del(opt.buildDir);
 });
 
 // Clear
 
-gulp.task('clear', function() {
+gulp.task('clear', function () {
   return cache.clearAll();
 });
 
 // Watch
 
-gulp.task('watch:html', function() {
+gulp.task('watch:html', function () {
   return gulp.watch('src/templates/**/*.pug', gulp.series('html'));
 });
 
-gulp.task('watch:styles', function() {
+gulp.task('watch:styles', function () {
   return gulp.watch('src/styles/**/*.less', gulp.series('styles'));
 });
 
-gulp.task('watch:scripts', function() {
+gulp.task('watch:scripts', function () {
   return gulp.watch('src/scripts/**/*.js', gulp.series('scripts'));
 });
 
-gulp.task('watch:copy', function() {
+gulp.task('watch:copy', function () {
   return gulp.watch([
     'src/*',
     '!src/images/*',
