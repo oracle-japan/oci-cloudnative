6825e8e0d12eadc32fa222504ec5a5b8b5997a12
shukawam <shukawam@gmail.com>
2025-03-04 09:54:16 +0900

    Added some resources for devops

--- deploy/complete/terraform/devops.tf
+++ deploy/complete/terraform/devops.tf
@@ -45,6 +45,13 @@ resource "oci_artifacts_container_repository" "mushop_registry" {
   is_public      = true
 }
 
+resource "oci_artifacts_repository" "mushop_helm_artifact" {
+  compartment_id  = var.compartment_ocid
+  is_immutable    = false
+  repository_type = "GENERIC"
+  display_name    = "mushop-helm-artifact"
+}
+
 resource "oci_devops_project" "mushop_devops_project" {
   compartment_id = var.compartment_ocid
   name           = "mushop"
@@ -67,7 +74,7 @@ resource "oci_devops_deploy_artifact" "mushop_orders_artifact" {
   deploy_artifact_source {
     deploy_artifact_source_type = "OCIR"
     image_uri = format(
-      "%s.ocir.io/%s/mushop/orders",
+      "%s.ocir.io/%s/mushop-orders:$${VERSION}",
       local.home_region_key,
       local.namespace
     )
@@ -82,7 +89,7 @@ resource "oci_devops_deploy_artifact" "mushop_api_artifact" {
   deploy_artifact_source {
     deploy_artifact_source_type = "OCIR"
     image_uri = format(
-      "%s.ocir.io/%s/mushop/api",
+      "%s.ocir.io/%s/mushop-api:$${VERSION}",
       local.home_region_key,
       local.namespace
     )
@@ -97,7 +104,22 @@ resource "oci_devops_deploy_artifact" "mushop_fulfillment_artifact" {
   deploy_artifact_source {
     deploy_artifact_source_type = "OCIR"
     image_uri = format(
-      "%s.ocir.io/%s/mushop/fulfillment",
+      "%s.ocir.io/%s/mushop-fulfillment:$${VERSION}",
+      local.home_region_key,
+      local.namespace
+    )
+  }
+}
+
+resource "oci_devops_deploy_artifact" "mushop_storefront_artifact" {
+  project_id                 = oci_devops_project.mushop_devops_project.id
+  display_name               = "mushop-storefront"
+  argument_substitution_mode = "SUBSTITUTE_PLACEHOLDERS"
+  deploy_artifact_type       = "DOCKER_IMAGE"
+  deploy_artifact_source {
+    deploy_artifact_source_type = "OCIR"
+    image_uri = format(
+      "%s.ocir.io/%s/mushop-storefront:$${VERSION}",
       local.home_region_key,
       local.namespace
     )
@@ -111,7 +133,7 @@ resource "oci_devops_deploy_artifact" "mushop_setup_artifact" {
   deploy_artifact_type       = "HELM_CHART"
   deploy_artifact_source {
     deploy_artifact_source_type = "HELM_CHART"
-    deploy_artifact_version     = "0.0.2"
+    deploy_artifact_version     = "$${VERSION}"
     chart_url = format(
       "oci://%s.ocir.io/%s/mushop/mushop-setup",
       local.home_region_key,
@@ -122,12 +144,12 @@ resource "oci_devops_deploy_artifact" "mushop_setup_artifact" {
 
 resource "oci_devops_deploy_artifact" "mushop_deploy_artifact" {
   project_id                 = oci_devops_project.mushop_devops_project.id
-  display_name               = "deploy"
+  display_name               = "mushop"
   argument_substitution_mode = "NONE"
   deploy_artifact_type       = "HELM_CHART"
   deploy_artifact_source {
     deploy_artifact_source_type = "HELM_CHART"
-    deploy_artifact_version     = "0.2.1"
+    deploy_artifact_version     = "$${VERSION}"
     chart_url = format(
       "oci://%s.ocir.io/%s/mushop/mushop",
       local.home_region_key,
@@ -136,56 +158,89 @@ resource "oci_devops_deploy_artifact" "mushop_deploy_artifact" {
   }
 }
 
+resource "oci_devops_deploy_artifact" "mushop_values_artifact" {
+  project_id                 = oci_devops_project.mushop_devops_project.id
+  display_name               = "values"
+  argument_substitution_mode = "SUBSTITUTE_PLACEHOLDERS"
+  deploy_artifact_type       = "GENERIC_FILE"
+  deploy_artifact_source {
+    deploy_artifact_source_type = "GENERIC_ARTIFACT"
+    deploy_artifact_version     = "$${VERSION}"
+    deploy_artifact_path        = "values"
+    repository_id               = oci_artifacts_repository.mushop_helm_artifact.id
+  }
+}
+
+resource "oci_devops_deploy_artifact" "mushop_storefront_values_artifact" {
+  project_id                 = oci_devops_project.mushop_devops_project.id
+  display_name               = "mushop-storefront-values"
+  argument_substitution_mode = "SUBSTITUTE_PLACEHOLDERS"
+  deploy_artifact_type       = "GENERIC_FILE"
+  deploy_artifact_source {
+    deploy_artifact_source_type = "GENERIC_ARTIFACT"
+    deploy_artifact_version     = "$${VERSION}"
+    deploy_artifact_path        = "mushop-storefront"
+    repository_id               = oci_artifacts_repository.mushop_helm_artifact.id
+  }
+}
+
+resource "oci_devops_deploy_artifact" "mushop_orders_values_artifact" {
+  project_id                 = oci_devops_project.mushop_devops_project.id
+  display_name               = "mushop-orders-values"
+  argument_substitution_mode = "SUBSTITUTE_PLACEHOLDERS"
+  deploy_artifact_type       = "GENERIC_FILE"
+  deploy_artifact_source {
+    deploy_artifact_source_type = "GENERIC_ARTIFACT"
+    deploy_artifact_version     = "$${VERSION}"
+    deploy_artifact_path        = "mushop-orders"
+    repository_id               = oci_artifacts_repository.mushop_helm_artifact.id
+  }
+}
+
+resource "oci_devops_deploy_artifact" "mushop_fulfillment_values_artifact" {
+  project_id                 = oci_devops_project.mushop_devops_project.id
+  display_name               = "mushop-fulfillment-values"
+  argument_substitution_mode = "SUBSTITUTE_PLACEHOLDERS"
+  deploy_artifact_type       = "GENERIC_FILE"
+  deploy_artifact_source {
+    deploy_artifact_source_type = "GENERIC_ARTIFACT"
+    deploy_artifact_version     = "$${VERSION}"
+    deploy_artifact_path        = "mushop-fulfillment"
+    repository_id               = oci_artifacts_repository.mushop_helm_artifact.id
+  }
+}
+
+resource "oci_devops_deploy_artifact" "mushop_api_values_artifact" {
+  project_id                 = oci_devops_project.mushop_devops_project.id
+  display_name               = "mushop-api-values"
+  argument_substitution_mode = "SUBSTITUTE_PLACEHOLDERS"
+  deploy_artifact_type       = "GENERIC_FILE"
+  deploy_artifact_source {
+    deploy_artifact_source_type = "GENERIC_ARTIFACT"
+    deploy_artifact_version     = "$${VERSION}"
+    deploy_artifact_path        = "mushop-api"
+    repository_id               = oci_artifacts_repository.mushop_helm_artifact.id
+  }
+}
+
+resource "oci_devops_trigger" "mushop_trigger" {
+  project_id     = oci_devops_project.mushop_devops_project.id
+  display_name   = "deploy-trigger"
+  repository_id  = oci_devops_repository.mushop_repository.id
+  trigger_source = "DEVOPS_CODE_REPOSITORY"
+  actions {
+    build_pipeline_id = oci_devops_build_pipeline.mushop_build_pipeline.id
+    type              = "TRIGGER_BUILD_PIPELINE"
+    filter {
+      events         = ["PUSH"]
+      trigger_source = "DEVOPS_CODE_REPOSITORY"
+    }
+  }
+}
+
 resource "oci_devops_build_pipeline" "mushop_build_pipeline" {
   project_id   = oci_devops_project.mushop_devops_project.id
   display_name = "mushop-build"
-}
-
-resource "oci_devops_build_pipeline_stage" "mushop_image_build" {
-  display_name              = "image-build"
-  build_pipeline_id         = oci_devops_build_pipeline.mushop_build_pipeline.id
-  build_pipeline_stage_type = "BUILD"
-  image                     = "OL7_X86_64_STANDARD_10"
-  build_pipeline_stage_predecessor_collection {
-    items {
-      id = oci_devops_build_pipeline.mushop_build_pipeline.id
-    }
-  }
-  build_source_collection {
-    items {
-      branch          = "main"
-      connection_type = "DEVOPS_CODE_REPOSITORY"
-      name            = "main"
-      repository_id   = oci_devops_repository.mushop_repository.id
-      repository_url  = oci_devops_repository.mushop_repository.http_url
-    }
-  }
-  build_spec_file = "build_spec_image.yaml"
-}
-
-resource "oci_devops_build_pipeline_stage" "mushop_image_ship" {
-  display_name              = "image-ship"
-  build_pipeline_id         = oci_devops_build_pipeline.mushop_build_pipeline.id
-  build_pipeline_stage_type = "DELIVER_ARTIFACT"
-  build_pipeline_stage_predecessor_collection {
-    items {
-      id = oci_devops_build_pipeline_stage.mushop_image_build.id
-    }
-  }
-  deliver_artifact_collection {
-    items {
-      artifact_name = "image_orders"
-      artifact_id   = oci_devops_deploy_artifact.mushop_orders_artifact.id
-    }
-    items {
-      artifact_name = "image_fulfillment"
-      artifact_id   = oci_devops_deploy_artifact.mushop_fulfillment_artifact.id
-    }
-    items {
-      artifact_name = "image_api"
-      artifact_id   = oci_devops_deploy_artifact.mushop_api_artifact.id
-    }
-  }
 }
 
 resource "oci_devops_build_pipeline_stage" "mushop_helm" {
@@ -195,7 +250,7 @@ resource "oci_devops_build_pipeline_stage" "mushop_helm" {
   image                     = "OL7_X86_64_STANDARD_10"
   build_pipeline_stage_predecessor_collection {
     items {
-      id = oci_devops_build_pipeline_stage.mushop_image_ship.id
+      id = oci_devops_build_pipeline.mushop_build_pipeline.id
     }
   }
   build_spec_file = "build_spec.yaml"
@@ -210,13 +265,46 @@ resource "oci_devops_build_pipeline_stage" "mushop_helm" {
   }
 }
 
+resource "oci_devops_build_pipeline_stage" "mushop_stack_ship" {
+  display_name              = "mushop-stack-ship"
+  build_pipeline_id         = oci_devops_build_pipeline.mushop_build_pipeline.id
+  build_pipeline_stage_type = "DELIVER_ARTIFACT"
+  build_pipeline_stage_predecessor_collection {
+    items {
+      id = oci_devops_build_pipeline_stage.mushop_helm.id
+    }
+  }
+  deliver_artifact_collection {
+    items {
+      artifact_name = "mushop-orders"
+      artifact_id   = oci_devops_deploy_artifact.mushop_orders_artifact.id
+    }
+    items {
+      artifact_name = "mushop-fulfillment"
+      artifact_id   = oci_devops_deploy_artifact.mushop_fulfillment_artifact.id
+    }
+    items {
+      artifact_name = "mushop-api"
+      artifact_id   = oci_devops_deploy_artifact.mushop_api_artifact.id
+    }
+    items {
+      artifact_name = "mushop-storefront"
+      artifact_id   = oci_devops_deploy_artifact.mushop_storefront_artifact.id
+    }
+    items {
+      artifact_name = "values"
+      artifact_id   = oci_devops_deploy_artifact.mushop_values_artifact.id
+    }
+  }
+}
+
 resource "oci_devops_build_pipeline_stage" "mushop_trigger_deployment_pipeline" {
   display_name              = "deploy-call"
   build_pipeline_id         = oci_devops_build_pipeline.mushop_build_pipeline.id
   build_pipeline_stage_type = "TRIGGER_DEPLOYMENT_PIPELINE"
   build_pipeline_stage_predecessor_collection {
     items {
-      id = oci_devops_build_pipeline_stage.mushop_helm.id
+      id = oci_devops_build_pipeline_stage.mushop_stack_ship.id
     }
   }
   deploy_pipeline_id             = oci_devops_deploy_pipeline.mushop_deploy_pipeline.id
