4a485def17499ae7343980fa482a6ddb579e4d91
shukawam <shukawam@gmail.com>
2025-03-06 12:55:42 +0900

    Scale down of cache cluster

--- deploy/complete/terraform/cache.tf
+++ deploy/complete/terraform/cache.tf
@@ -1,8 +1,8 @@
 resource "oci_redis_redis_cluster" "mushop_cache" {
   compartment_id     = var.compartment_ocid
   display_name       = "mushop-cache"
-  node_count         = 3
-  node_memory_in_gbs = 4
+  node_count         = 1
+  node_memory_in_gbs = 2
   software_version   = "REDIS_7_0"
   subnet_id          = oci_core_subnet.mushop_node_subnet.id
 }
