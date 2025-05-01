2e4757479736a3cb68965e7be9ac32010c8748f6
cyberblack28 <y.ichikawa2014@gmail.com>
2025-05-01 23:57:47 +0900

    prepare script update

--- deploy/complete/prepare/prepare-logging.sh
+++ deploy/complete/prepare/prepare-logging.sh
@@ -7,5 +7,5 @@ echo "Compartment ID: ${compartment_id}"
 # Create Dynamic-Group
 oci iam dynamic-group create --name OCI_Logging_Dynamic_Group --description OCI_Logging_Dynamic_Group --matching-rule "any {instance.compartment.id = '${compartment_id}'}"
 
-#Create Policy
+# Create Policy
 oci iam policy create --name OCI_Logging_Policy --description OCI_Logging_Policy --compartment-id "${compartment_id}" --statements '["Allow dynamic-group Default/OCI_Logging_Dynamic_Group to use log-content in compartment id '${compartment_id}'"]'
