5b782f6574f3eac7ff598841ba61499591aeefdb
cyberblack28 <y.ichikawa2014@gmail.com>
2025-05-08 23:30:55 +0900

    script delete

--- deploy/complete/prepare/prepare-apm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Get CompartmentID
-compartment_id=$(oci iam compartment list --include-root --query "data[*].id" --raw-output | sed '/^\[/d;/^\]/d;s/^[[:space:]]*//;s/"//g')
-echo "Compartment ID: ${compartment_id}"
-
-# Create Policy
-oci iam policy create --name OCI_APM_Policy --description OCI_APM_Policy --compartment-id "${compartment_id}" --statements '["Allow group APM-Admins to manage apm-domains in compartment id '${compartment_id}'"]'
